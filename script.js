3b86ce8555b7f9b169c1e6fc6a15e574e5ef9293
Esther Pautrel <esther.pautrel@etu.minesparis.psl.eu>
2025-03-23 22:36:14 +0100

    commit

--- script.js
+++ script.js
@@ -113,12 +113,5 @@ window.addEventListener("load", () => {
     
 
 //ETAPES A FAIRE
-//pouvoir clicker sur les chiffres : console.log => attraper evt click
-//affichage là haut : nb à un chiffre
-//puis nb à deux chiffres
-//après faire le +
-// que seul 5 s'affiche quand y'a 000005
-//faire le clear AC
-
-//attention: on termine par le décimal, le . ce n'est que pour la fin!!
+//on termine par le décimal, le . ce n'est que pour la fin!!
 //commiter à chaque fois à partir de chaque étape
